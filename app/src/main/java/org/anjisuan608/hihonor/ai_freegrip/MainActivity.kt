d02cc38d644132617b1011f4dfdffece83a0d4fc
anjisuan608 <anjisuan608@petalmail.com>
2026-09-22 19:05:07 +0800

    初始化项目

---
+++ app/src/main/java/org/anjisuan608/hihonor/ai_freegrip/MainActivity.kt
@@ -0,0 +1,47 @@
+package org.anjisuan608.hihonor.ai_freegrip
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.anjisuan608.hihonor.ai_freegrip.ui.theme.AIFreegripTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AIFreegripTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AIFreegripTheme {
+        Greeting("Android")
+    }
+}
